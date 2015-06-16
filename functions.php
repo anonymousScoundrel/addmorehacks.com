ceccf96fbf0986039b30f515d36073c9430dafdc
Theo Coukoulis <root@addmorehacks.com>
2015-06-15 23:30:35 -0500

    Enqueued css and js

--- functions.php
+++ functions.php
@@ -1,3 +1,31 @@
 <?php
 
+    /**
+     * Load Enqueued Scripts in the Footer
+     *
+     * Automatically move JavaScript code to page footer, speeding up page loading time.
+     */
+    function footer_enqueue_scripts() {
+        remove_action('wp_head', 'wp_print_scripts');
+        remove_action('wp_head', 'wp_print_head_scripts', 9);
+        remove_action('wp_head', 'wp_enqueue_scripts', 1);
+        add_action('wp_footer', 'wp_print_scripts', 5);
+        add_action('wp_footer', 'wp_enqueue_scripts', 5);
+        add_action('wp_footer', 'wp_print_head_scripts', 5);
+    }
+    add_action('after_setup_theme', 'footer_enqueue_scripts');
 
+    function morehacks_scripts () {
+        wp_enqueue_style(
+            "morehackscss",
+            get_template_directory_uri() . "/style.css"
+        );
+        wp_enqueue_script(
+            "morehacksjs",
+            get_template_directory_uri() . "/morehacks.min.js",
+            array("jquery"),
+            false,
+            true
+        );
+    }
+    add_action("wp_enqueue_scripts", "morehacks_scripts");
--- js/dev.morehacks.js
+++ js/dev.morehacks.js
@@ -1,6 +1,16 @@
 (function ($) {
     "use strict";
 
+    /**
+      * IE safe console.log, not that you should leave log messages around
+      */
+    $.log = function (data) {
+        if (window.console) {
+            console.log(data);
+        }
+    };
+
     $(document).ready(function () {
+        $.log("loaded and rocking...");
     });
 }(jQuery, undefined));
